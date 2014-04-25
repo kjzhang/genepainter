2a7bfaa7252dba0d948e47cdfe965270437f0812
Kevin Zhang <kjzhang92@gmail.com>
2014-04-24 21:52:58 -0400

    some refactoring

--- genepainter.py
+++ genepainter.py
@@ -1,6 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 import scipy.interpolate
 import time
 
@@ -8,73 +9,78 @@ from PIL import Image
 from matplotlib.backends.backend_agg import RendererAgg
 from matplotlib.lines import Line2D
 
+epsilon = 10
+objects = 10
+population_size = 10
+mutation_rate = 0.5
+min_iter = 10
+num_spline_points = 4
+
+class GeneStroke(object):
+	def __init__(self, sx, sy, color, alpha, width):
+		if sx.ndim != 1 or sy.ndim != 1 or sx.size != sy.size:
+			raise Exception()
+
+		self.sx = sx
+		self.sy = sy
+		self.color = color
+		self.alpha = alpha
+		self.width = width
+
+	def spline(self, resolution=1000, interpolation='cubic'):
+		t = np.linspace(0, 1, self.sx.size)
+		sx = scipy.interpolate.interp1d(t, self.sx, kind=interpolation)
+		sy = scipy.interpolate.interp1d(t, self.sy, kind=interpolation)
+		st = np.linspace(0, 1, resolution)
+		return sx(st), sy(st)
+
+	def render(self, r):
+		sx, sy = self.spline()
+		stroke = Line2D(sx, sy, color=self.color, alpha=self.alpha, lw=self.width, solid_capstyle="round")
+		stroke.draw(r)
+
+	@classmethod
+	def random(cls, shape):
+		ymax, xmax = shape
+		pointsx = np.random.random(num_spline_points) * xmax
+		pointsy = np.random.random(num_spline_points) * ymax
+
+		color = tuple(np.random.random(3))
+		alpha = np.random.uniform(0.25, 1)
+		width = random.randint(0, min(ymax, xmax) / 4)
+
+		return cls(pointsx, pointsy, color, alpha, width)
+
+class GeneImage(object):
+	def __init__(self, shape):
+		self.shape = shape
+		self.strokes = []
+
+		for _ in xrange(10):
+			self.strokes.append(GeneStroke.random(self.shape))
+
+	def render(self):
+		w, h = self.shape
+		r = RendererAgg(w, h, 72)
+		arr = np.frombuffer(r.buffer_rgba(), np.uint8)
+		arr.shape = r.height, r.width, -1
+
+		t = time.time()
+		for stroke in self.strokes:
+			stroke.render(r)
+		print time.time() - t
+
+		return np.array(arr, dtype=np.float32) / 255
+
 class GenePainter(object):
 	def __init__(self, source):
 		self.source = source
 
-	def render(self):
-		output = np.zeros(self.source.shape, dtype=np.float32)
-		Ny, Nx = output.shape[0], output.shape[1]
-
-		#x = np.array([5, 10, 15, 20, 5, 5])
-		#y = np.array([5, 5, 20, 15, 10, 30])
-
-		x = np.array(np.random.random(4) * 128, dtype=np.float32)
-		y = np.array(np.random.random(4) * 128, dtype=np.float32)
-		sx, sy = spline(x, y, 1000)
-
-		t = time.time()
-		for yi in xrange(Ny):
-			for xi in xrange(Nx):
-				d = min_distance(sx, sy, xi, yi)
-				if d < 10.: # radius
-					output[yi, xi, :] = np.array([1, 1, 0, 0.5])
-		print time.time() - t
-
-
-		w, h = 256, 256
-		r = RendererAgg(256, 256, 72)
-		arr = np.frombuffer(r.buffer_rgba(), np.uint8)
-		arr.shape = r.height, r.width, -1
-		t = np.linspace(0, 2*np.pi, 100)
-		x = np.sin(2*t) * w*0.45 + w*0.5
-		y = np.cos(3*t) * h*0.45 + h*0.5
-
-		t = time.time()
-		for _ in xrange(10):
-
-			x = np.array(np.random.random(4) * 256, dtype=np.float32)
-			y = np.array(np.random.random(4) * 256, dtype=np.float32)
-			sx, sy = spline(x, y, 1000)
-
-			line = Line2D(sx, sy, linewidth=50, color=(1.0, 0.0, 0.0), alpha=0.3)
-			line.draw(r)
-		print time.time() - t
-		# plt.imsave("test.png", arr)
-
-		# t = time.time()
-		# for _ in xrange(100):
-		# 	plt.plot(sx, sy, label='spline', linewidth=10, aa=False, solid_capstyle="round")
-		# print time.time() - t
-
-		plt.imshow(arr, interpolation='none')
-		plt.show()
-
-	def score(self, image):
+	def fitness(self, image):
 		return np.linalg.norm(self.source - image, 2)
 
-def spline(x, y, n):
-	if x.ndim != 1 or y.ndim != 1 or x.size != y.size:
-		raise Exception()
-
-	t = np.linspace(0, 1, x.size)
-
-	sx = scipy.interpolate.interp1d(t, x, kind='cubic')
-	sy = scipy.interpolate.interp1d(t, y, kind='cubic')
-
-	st = np.linspace(0, 1, n)
-
-	return sx(st), sy(st)
+	def paint(self):
+		pass
 
 def min_distance(sx, sy, px, py):
 	dx = sx - px
@@ -85,28 +91,15 @@ def min_distance(sx, sy, px, py):
 def read_image(file):
 	image_raw = Image.open(file)
 	image_raw.load()
-	# return np.array(image_raw, dtype=np.float32)
-	image_rgb = Image.new('RGB', image_raw.size)
-	image_rgb.paste(image_raw, None)
-	return np.array(image_rgb, dtype=np.float32)
-
-class GeneImage(object):
-	def __init__(self):
-		self.strokes = []
-
-class GeneStroke(object):
-	def __init__(self, x, y, radius):
-		if x.ndim != 1 or y.ndim != 1 or x.size != y.size:
-			raise Exception()
-
-		self.x = x
-		self.y = y
-		self.radius = radius
+	return np.array(image_raw, dtype=np.float32)
 
 if __name__ == "__main__":
 
 	# source = read_image('ML129.png')
 	source = np.zeros((256, 256, 4), dtype=np.float32)
 
-	p = GenePainter(source)
-	p.render()
+	p = GeneImage((256, 256))
+	image = p.render()
+
+	plt.imshow(image, interpolation='none')
+	plt.show()
